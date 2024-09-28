0010e7688cebe96527cf42b03cc5cf5bd464487e
Pacifique Twagirayesu <paccy@Pacifiques-MacBook-Air.local>
2024-09-28 22:26:56 +0800

    springdoc openapi docs code

--- api/src/main/java/com/pacifique/microservices/api/composite/product/ReviewSummary.java
+++ api/src/main/java/com/pacifique/microservices/api/composite/product/ReviewSummary.java
@@ -3,6 +3,8 @@ package com.pacifique.microservices.api.composite.product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 @Data
 @NoArgsConstructor(force = true)
--- microservices/product-composite-service/src/main/java/com/pacifique/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/pacifique/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,5 +1,11 @@
 package com.pacifique.microservices.composite.product;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,14 +15,62 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @ComponentScan("com.pacifique")
 public class ProductCompositeServiceApplication {
+    @Value("${api.common.version}")
+    String apiVersion;
+    @Value("${api.common.title}")
+    String apiTitle;
+    @Value("${api.common.description}")
+    String apiDescription;
+    @Value("${api.common.termsOfService}")
+    String apiTermsOfService;
+    @Value("${api.common.license}")
+    String apiLicense;
+    @Value("${api.common.licenseUrl}")
+    String apiLicenseUrl;
+    @Value("${api.common.externalDockDesc}")
+    String apiExternalDockDesc;
+    @Value("${api.common.externalDoUrl}")
+    String apiExternalDoUrl;
+    @Value("${api.common.contact.name}")
+    String apiContactName;
+    @Value("${api.common.contact.url}")
+    String apiContactUrl;
+    @Value("${api.common.contact.email}")
+    String apiContactEmail;
 
-	@Bean
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCompositeServiceApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductCompositeServiceApplication.class, args);
-	}
+    /**
+     * We will expose on $HOST:$PORT/swagger-ui.html
+     *
+     * @return the common api documentation
+     */
+
+    @Bean
+    public OpenAPI getOpenApiDocumentation() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(apiTitle)
+                        .description(apiDescription)
+                        .version(apiVersion)
+                        .contact(new Contact()
+                                .name(apiContactName)
+                                .url(apiContactUrl)
+                                .email(apiContactEmail))
+                        .termsOfService(apiTermsOfService)
+                        .license(new License()
+                                .name(apiLicense)
+                                .url(apiLicenseUrl)))
+                .externalDocs(new ExternalDocumentation()
+                        .description(apiExternalDockDesc)
+                        .url(apiExternalDoUrl));
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
